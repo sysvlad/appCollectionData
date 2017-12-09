3c0ed8eb5bb9bde278f86da48b8cdc69c3b10939
Vlad <sysvladsysvlad@gmail.com>
2017-12-09 10:42:19 +0300

    Initial Commit

---
+++ balinaSoft/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  balinaSoft
+//
+//  Created by Vlad Sys on 12/9/17.
+//  Copyright © 2017 Vlad Sys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
