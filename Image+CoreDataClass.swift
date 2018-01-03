32e0d40144b087a1a8ebcf0de5b2251c18ff0e86
Vlad <sysvladsysvlad@gmail.com>
2018-01-03 15:37:12 +0300

    first commit

---
+++ Image+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Image+CoreDataClass.swift
+//  
+//
+//  Created by Vlad Sys on 12/30/17.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Image)
+public class Image: NSManagedObject {
+
+}
